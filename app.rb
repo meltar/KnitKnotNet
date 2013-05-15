bc8254645046b4fe743bbf1e2c5284e43e4db325
melissa <meltar@gmail.com>
2013-05-15 01:04:15 -0400

    Comment out contact section

--- app.rb
+++ app.rb
@@ -14,16 +14,16 @@ get "/about" do
   erb :about
 end
 
-get "/contact" do
-  erb :contact
-end
+#get "/contact" do
+#  erb :contact
+#end
 
-post "/contactme" do
-  name = params[:name]
-  mail = params[:mail]
-  body = params[:body]
+#post "/contactme" do
+#  name = params[:name]
+#  mail = params[:mail]
+#  body = params[:body]
 
-  Pony.mail(to: 'knitknotnet@gmailc.com', from: "#{mail}", subject: "Contact form submission from #{name}", body: "#{body}")
+#  Pony.mail(to: 'knitknotnet@gmail.com', from: "#{mail}", subject: "Contact form submission from #{name}", body: "#{body}")
 
-  erb :contactme
-end
+#  erb :contactme
+#end
